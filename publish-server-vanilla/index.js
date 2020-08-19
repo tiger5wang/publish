691a2ca36a8d879b38af0865a37aa5673b5bf47c
wangyuhu <826969978@qq.com>
2020-08-19 23:18:48 +0800

    发布系统

--- publish-server-vanilla/index.js
+++ publish-server-vanilla/index.js
@@ -1,21 +1,28 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+
+const unzip = require('unzipp')
+const unzipper = require('unzipper')
+
 
 // Create an HTTP tunneling proxy
 const server = http.createServer((req, res) => {
-    let matched = req.url.match(/filename=([^&]+)/);
-    let filename = matched && matched[1];
-    if (!filename) {
-        return;
-    }
-    let writeStream = fs.createWriteStream('../server/public/' + filename);
-    req.pipe(writeStream);  // 等效于下面注释
+    // let matched = req.url.match(/filename=([^&]+)/);
+    // let filename = matched && matched[1];
+    // if (!filename) {
+    //     return;
+    // }
+    // let writeStream = fs.createWriteStream('../server/public/' + filename);
+    // req.pipe(writeStream);  // 等效于下面注释
     // req.on('data', trunk => {
     //     writeStream.write(trunk)
     // });
     // req.on('end', trunk => {
     //     writeStream.end(trunk)
     // })
+
+    let writeStream = unzipper.Extract({path: '../server/public'});
+    req.pipe(writeStream);
 
     req.on('end', () => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
