83f36a3f4e66d0a1d6ada6400ba1eb7513f23fde
wangyuhu <826969978@qq.com>
2020-08-27 18:02:32 +0800

    OAuth

--- publish-server-vanilla/index.js
+++ publish-server-vanilla/index.js
@@ -1,7 +1,8 @@
 const http = require('http');
-// const fs = require('fs');
-// const unzip = require('unzip');
-// const unzipper = require('unzipper')
+const https = require('https');
+const fs = require('fs');
+const unzip = require('unzip');
+const unzipper = require('unzipper')
 
 
 // Create an HTTP tunneling proxy
@@ -10,14 +11,67 @@ const server = http.createServer((req, res) => {
     if (req.url.match(/^\/auth/)) {
         return auth(req, res)
     }
-
+	
+	if (!req.url.match(/^\/?/)) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('not found');
+		return
+	}
+	
+	const options = {
+		hostname: 'api.github.com',
+		port: 443,
+		path: `/user`,
+		method: 'GET',
+        headers: {
+			Authorization: `token ${req.headers.token}`,
+            "User-Agent": "publish-server"
+        }
+	};
+	
+	const request = https.request(options, (response) => {
+		// console.log('statusCode:', res.statusCode);
+		// console.log('headers:', res.headers);
+		let body = "";
+		response.on('data', (d) => {
+			console.log('dddd', d.toString());  // 获取的 user 信息
+            body += d.toString()
+		});
+		
+		response.on('end', () => {
+		    let user = JSON.parse(body);
+		    console.log(user)
+            
+            // 此处进行权限检查
+			
+			// let writeStream = unzipper.Extract({path: '../server/public'});
+			// req.on('data', trunk => {
+			//     writeStream.write(trunk)
+			// });
+			// req.on('end', trunk => {
+			//     writeStream.end(trunk)
+			// })
+			
+            // 接收数据
+			req.on('end', () => {
+				res.writeHead(200, { 'Content-Type': 'text/plain' });
+				res.end('okay');
+			})
+        })
+	});
+	
+	request.on('error', (e) => {
+		console.error(e);
+	});
+	request.end();
+    
     // let matched = req.url.match(/filename=([^&]+)/);
     // let filename = matched && matched[1];
     // if (!filename) {
     //     return;
     // }
     // let writeStream = fs.createWriteStream('../server/public/' + filename);
-    // req.pipe(writeStream);  // 等效于下面注释
+    // req.pipe(writeStream);  // 等效于下面 6 行注释
     // req.on('data', trunk => {
     //     writeStream.write(trunk)
     // });
@@ -36,10 +90,52 @@ const server = http.createServer((req, res) => {
 });
 
 function auth(req, res) {
-    let code = req.url.match(/code=([^&]+)/)[1];
-    console.log(code)
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('okay');
+    let code = req.url.match(/code=([^&]+)/)[1];   // 匹配 code
+	let state = 'abc123';
+	let client_secret = '2145fce43a48e148c6893cff6a225e84f405c229';
+	let client_id = 'Iv1.5dd238130b37f0db';
+	let redirect_uri = encodeURIComponent('http://localhost:8081/auth');
+	
+	let params = `code=${code}&state=${state}&client_secret=${client_secret}&client_id=${client_id}&redirect_uri=${redirect_uri}`;
+	let url = `https://github.com/login/oauth/access_token?${params}`;
+	
+	const options = {
+		hostname: 'github.com',
+		port: 443,
+		path: `/login/oauth/access_token?${params}`,
+		method: 'POST'
+	};
+	
+	const request = https.request(options, (response) => {
+		// console.log('statusCode:', res.statusCode);
+		// console.log('headers:', res.headers);
+		
+		response.on('data', (d) => {
+			// console.log('dddd', d.toString())
+            let result = d.toString().match(/access_token=([^&]+)/);   // 匹配 access_token
+            if(result) {
+				let token = result[1];
+	
+				res.writeHead(200, {
+					'access_token': token,
+					'Content-Type': 'text/html'
+				});
+				res.end(`<a href="http://localhost:8080/publish?token=${token}">publish</a>`);
+            } else {
+				res.writeHead(200, {
+					'Content-Type': 'text/plain'
+				});
+				res.end('error');
+            }
+			
+		});
+	});
+	
+	request.on('error', (e) => {
+		console.error(e);
+	});
+	request.end();
+	
 }
 
 server.listen(8081);
--- publish-tool/publish.js
+++ publish-tool/publish.js
@@ -6,13 +6,66 @@ var child_process = require('child_process');
 
 // create a file to stream archive data to.
 var output = fs.createWriteStream(__dirname + '/example.zip');
-
-
 const postData = querystring.stringify({
     'msg': 'Hello World! haha'
 });
 
-let options;
+// 三：
+let redirect_uri = encodeURIComponent('http://localhost:8081/auth')
+child_process.exec(`start chrome https://github.com/login/oauth/authorize?client_id=Iv1.5dd238130b37f0db&redirect_uri=${redirect_uri}&scope=read%3Auser&state=abc123`);
+
+const server = http.createServer((request, res) => {
+    console.log(request.url)
+    console.log('real publish!!!')
+    const token = request.url.match(/token=([^&]+)/)[1];
+	
+	let file = './package';
+	let options = {
+		host: 'localhost',
+		port: 8081,
+		path: '/?filename=package.zip',
+		method: 'POST',
+		headers: {
+		    'token': token,
+			'Content-Type': 'application/x-www-form-urlencoded',
+			// 'Content-Length': 0  // 这个要去掉
+		}
+	};
+	
+	const req = http.request(options, (res) => {
+		console.log(`STATUS: ${res.statusCode}`);
+		console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+	});
+	req.on('error', (e) => {
+		console.error(`problem with request: ${e.message}`);
+	});
+	
+	var archive = archiver('zip', {
+		zlib: { level: 9 } // Sets the compression level.
+	});
+	
+	archive.directory(file, false);
+
+// archive.pipe(fs.createWriteStream('./package.zip'))
+// archive.on('end', () => {
+//     console.log('end')
+// })
+	
+	archive.pipe(req);
+	archive.on('end', () => {
+		req.end();
+		let redirect_uri = encodeURIComponent('http://localhost:8081/auth')
+		child_process.exec(`start chrome https://github.com/login/oauth/authorize?client_id=Iv1.5dd238130b37f0db&redirect_uri=${redirect_uri}&scope=read%3Auser&state=abc123`)
+	})
+	archive.finalize();
+    
+});
+
+server.listen(8080);
+
+// 二： GitHub OAuth 验证
+
+/*let options;
 let file = './package';
 // fs.stat(file,  (error, stat) => {
     options = {
@@ -51,8 +104,10 @@ archive.on('end', () => {
     let redirect_uri = encodeURIComponent('http://localhost:8081/auth')
     child_process.exec(`start chrome https://github.com/login/oauth/authorize?client_id=Iv1.5dd238130b37f0db&redirect_uri=${redirect_uri}&scope=read%3Auser&state=abc123`)
 })
-archive.finalize();
+archive.finalize();*/
 
+
+// 一：读取文件并写入本地 zip
 
 //     const req = http.request(options, (res) => {
 //         console.log(`STATUS: ${res.statusCode}`);
@@ -81,6 +136,7 @@ archive.finalize();
 // })
 
 
+// 一：读取文件并写入本地 jpg
 
 // const req = http.request(options, (res) => {
 //     console.log(`STATUS: ${res.statusCode}`);
